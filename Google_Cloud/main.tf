9ca3d30d21b917ece18c3cd1f921de6ba8d3e763
emiliska <33560517+emiliska@users.noreply.github.com>
2019-09-19 19:34:18 -0400

    update script

--- Google_Cloud/main.tf
+++ Google_Cloud/main.tf
@@ -1,28 +1,29 @@
 provider "google" {
-  project     = "iasa-training"
-  region      = "us-east1"
+  project     = "iasa-training" // GCP Project Name
+  region      = "us-central" // GCP project region
+  zone        = "us-central1-c"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  count        = "30"
-  name         = "ubuntu1804"
-  machine_type = "f1-micro"
+  count        = "30" // number of vm instances being set
+  name         = "ubuntu${count.index+1}"
+  machine_type = "g1-small" // 0.5 vCPUs 1.7 GB RAM
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts" // ubuntu 18.04 LTS
     }
   }
 
   network_interface {
     # A default network is created for all GCP projects
-    network       = "${google_compute_network.vpc_network.self_link}"
+    network       = "${google_compute_network.vpc_network.self_link}" // does magic
     access_config {
     }
   }
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+  name                    = "terraform-network"  
+  auto_create_subnetworks = "true" 
 }
