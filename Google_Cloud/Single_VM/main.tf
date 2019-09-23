2b806c52f799dfb6c360385ccd635f91b22500d0
Captain-K-nuckles <cmeyer25@emich.edu>
2019-09-23 19:03:31 -0400

    Updated main.tf

--- Google_Cloud/Single_VM/main.tf
+++ Google_Cloud/Single_VM/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  count        = "30" // number of vm instances being set
+//  count        = "30" // number of vm instances being set
   name         = "ubuntu${count.index+1}"
   machine_type = "g1-small" // 0.5 vCPUs 1.7 GB RAM
 
