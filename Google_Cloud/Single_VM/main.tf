20f77bdd727bc3dd4de48582c2cd649b7bcb109e
Captain-K-nuckles <cmeyer25@emich.edu>
2019-09-26 21:40:14 -0400

    Updated for accounts.json

--- Google_Cloud/Single_VM/main.tf
+++ Google_Cloud/Single_VM/main.tf
@@ -1,11 +1,12 @@
 provider "google" {
+  credentials = "${file("account.json")}"
   project     = "iasa-training" // GCP Project Name
   region      = "us-central" // GCP project region
   zone        = "us-central1-c"
 }
 
 resource "google_compute_instance" "vm_instance" {
-//  count        = "30" // number of vm instances being set
+  count        = "1" // number of vm instances being set
   name         = "ubuntu${count.index+1}"
   machine_type = "g1-small" // 0.5 vCPUs 1.7 GB RAM
 
